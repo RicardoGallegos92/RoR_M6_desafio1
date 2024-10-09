b8a27e373f05d2756800201442c98bf494255c09
Ricardo Gallegos <ricardo.gallegos@usach.cl>
2024-10-09 19:38:33 -0300

    boton volver arriba

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "clients#index"
+  root "movies#index"
   # añadimos una redireccion default
-  get "/index", to: "clients#index"
+  get "/index", to: "movies#index"
 end
